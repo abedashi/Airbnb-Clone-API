6a9a895250d67f43336ff548d26323adab34bbb9
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-11-18 21:46:55 +0200

    database connction

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+    class Database {
+        // DB Params
+        private $host = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $db = "RSP";
+        private $conn;
+
+        //DB Connect
+        public function connect() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db);
+            } catch(Exception $e) {
+                echo 'Conection Error: ' . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+    }
+?>
