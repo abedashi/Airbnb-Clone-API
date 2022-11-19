32d5152f92519cffcc09ce900dc83af8a66d9c9a
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-11-19 02:20:10 +0200

    User table declaration for login & register

--- models/User.php
+++ models/User.php
@@ -5,11 +5,16 @@ class User {
 
   // User props
   public $id;
-  public $role;
-  public $fname;
-  public $lname;
-  public $email;
+  public $username;
   public $password;
+  public $passwordConfirm;
+  public $image;
+  public $job;
+  public $about;
+  public $location;
+  public $language;
+  public $joined_in;
+  public $term;
 
   public function __construct($db) {
     $this->conn = $db;
@@ -19,28 +24,30 @@ class User {
 
     // Check if email is taken
     $query = $this->conn->prepare(
-      "SELECT email FROM {$this->table} WHERE email = ?"
+      "SELECT username FROM {$this->table} WHERE username = ?"
     );
 
     // Bind email and execute
-    $query->bind_param("s", $this->email);
+    $query->bind_param("s", $this->username);
     $query->execute();
     $query->store_result();
 
     if ($query->num_rows > 0) {
       throw new Exception("Email already exists");
     }
-
+    if ($this->$password !== $this->$passwordConfirm) {
+      throw new Exception("Password and Password Confirmation didn't matched");
+    }
     // Create query and prepare to register user
     $query = $this->conn->prepare(
-      "INSERT INTO {$this->table} VALUES(NULL,?, ?, ?, ?, ?)"
+      "INSERT INTO {$this->table} VALUES (NULL, ?, ?, ?)"
     );
 
     // Hash password
     $this->password = password_hash($this->password, PASSWORD_BCRYPT);
 
     // Bind data
-    $query->bind_param("sssss", $this->role ,$this->fname, $this->lname, $this->email, $this->password);
+    $query->bind_param("ssb", $this->username ,$this->password, $this->term);
 
     // Execute Query
     if ($query->execute()) {
@@ -51,40 +58,39 @@ class User {
   }
 
   public function login() {
+    // Prepare statment
     $query = $this->conn->prepare(
-      "SELECT id,role, password, fname, lname FROM {$this->table} WHERE email = ?"
+      "SELECT id, password, image, job, about, location, language, joined_in FROM {$this->table} WHERE username = ?"
     );
 
-    $query->bind_param("s", $this->email);
+    // Bind username value to query
+    $query->bind_param("s", $this->username);
+
+    // Execute Query 
     $query->execute();
+
+    // Get store the result 
     $result = $query->get_result();
     
+    // Check if this username exist in users table
     if ($result->num_rows === 0) {
       throw new Exception("Invalid Credentials");
     }
     
     $result = $result->fetch_assoc();
 
+    // Check if password and username matched
     if (password_verify($this->password, $result["password"])) {
       $this->id = $result["id"];
-      $this->role = $result["role"];
-      $this->fname = $result["fname"];
-      $this->lname = $result["lname"];
+      $this->job = $result["job"];
+      $this->image = $result["image"];
+      $this->about = $result["about"];
+      $this->location = $result["location"];
+      $this->language = $result["language"];
+      $this->joined_in = $result["joined_in"];
       return true;
     }
 
-    return false;
-  }
-
-  public function updateRole() {
-    $query = $this->conn->prepare(
-      "UPDATE users SET role = ? WHERE id = ?"
-    );
-
-    $query->bind_param('si', $this->role, $this->id);
-    if ($query->execute()) {
-      return true;
-    }
     return false;
   }
 }
