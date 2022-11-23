42f5e0be8aa10a6830b47e2916ef58e7262ff059
Habibashi <96149736+habibashi@users.noreply.github.com>
2022-11-23 18:54:21 +0200

    check error

--- api/users/login.php
+++ api/users/login.php
@@ -4,7 +4,7 @@
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: POST');
   header('Access-Control-Allow-Headers: Accept, Origin, Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
-  // header('Access-Control-Allow-Headers: ');
+
   if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit('ok');
   }
