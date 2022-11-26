2f1bbc7fa638c6b806aade2669e2187be051f87f
Habibashi <96149736+habibashi@users.noreply.github.com>
2022-11-26 21:28:23 +0200

    get profile data

---
+++ api/users/get_profile.php
@@ -0,0 +1,35 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: GET');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit('ok');
+  }
+
+  require_once '../../config/Database.php';
+  require_once '../../models/User.php';
+
+    // Connect db
+    $database = new Database();
+    $db = $database->connect();
+  
+    $user = new User($db);
+
+    try {
+      require '../../config/protect.php';
+      
+      $user->id = $userId;
+      
+      if($result = $user->getProfile()) {
+        http_response_code(200);
+        echo json_encode($result->fetch_assoc()); 
+      }
+
+    } catch (Exception $e) {
+      http_response_code(401);
+      echo json_encode(
+        array('message' => $e->getMessage())
+      );
+    }
