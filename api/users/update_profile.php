b5762eb7834674254804466df6ae0f9ca51df0b4
Habibashi <96149736+habibashi@users.noreply.github.com>
2022-11-26 21:29:28 +0200

    update profile data

---
+++ api/users/update_profile.php
@@ -0,0 +1,45 @@
+<?php 
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: PUT');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit('ok');
+  }
+
+  require_once '../../config/Database.php';
+  require_once '../../models/User.php';
+
+  // Connect db
+  $database = new Database();
+  $db = $database->connect();
+
+  $user = new User($db);
+
+  try {
+    // Protect route
+    require '../../config/protect.php';
+
+    $data = json_decode(file_get_contents("php://input"));
+
+    $user->id = $userId;
+    $user->image = $data->image;
+    $user->job = $data->job;
+    $user->about = $data->about;
+    $user->location = $data->location;
+    $user->language = $data->language;
+
+    if ($user->updateProfile()) {
+      http_response_code(204);
+    }
+    else {
+      throw new Exception("Update Failed");
+    }
+
+  } catch (Exception $e) {
+    http_response_code(400);
+    echo json_encode([
+      "message" => $e->getMessage()
+    ]);
+  }
+?>
