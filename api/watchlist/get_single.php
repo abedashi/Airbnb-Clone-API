21133dea83ab4ca38d4e44d445d01d23cc243339
Habibashi <96149736+habibashi@users.noreply.github.com>
2022-11-26 21:26:43 +0200

    get a single watchlist

--- api/watchlist/get_single.php
+++ api/watchlist/get_single.php
@@ -4,6 +4,9 @@
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: GET');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit('ok');
+  }
 
   require_once '../../config/Database.php';
   require_once '../../models/Watchlist.php';
@@ -17,19 +20,12 @@
 
     require '../../config/protect.php';
     // get id
-    // change from htmlspecialchars to isset if id or die 
-    //....../?rooms=2&abc=3...
 
     $watchlist->userId = $userId;
-    $watchlist->appartment_id = isset($_GET["appartment_id"]) ? $_GET["appartment_id"] : die();
-
+  
     if($result = $watchlist->getSingle()) {
-      $response = [];
-      while ($row = $result->fetch_assoc()) {
-        $response[] = $row;
-      }
       http_response_code(200);
-      echo json_encode($response); 
+      echo json_encode($result->fetch_assoc()); 
     }
 
   } catch (Exception $e) {
@@ -38,4 +34,3 @@
       array('message' => $e->getMessage())
     );
   }
-?>
