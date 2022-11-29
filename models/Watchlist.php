997104425e640a21573488190325ebfbb1dd29a0
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-11-29 02:46:12 +0200

    resolve delete query

--- models/Watchlist.php
+++ models/Watchlist.php
@@ -39,7 +39,7 @@ class WatchList
                  JOIN coordinates ON appartments_list.id = coordinates.appartment_id
                  JOIN offers ON appartments_list.id = offers.appartment_id
                  JOIN images ON appartments_list.id = images.appartment_id
-                 WHERE watch_list.userId = ?"
+                 WHERE watch_list.userId = ? ORDER BY watch_list.id DESC"
         );
         $query->bind_param('i', $this->userId);
         $query->execute();
@@ -51,15 +51,7 @@ class WatchList
     public function getSingle()
     {
         $query = $this->conn->prepare(
-            "SELECT watch_list.appartment_id, watch_list.userId, appName, address, guests, price, bedroom, bed, bath, lat, lng,
-              image1, image2, image3, image4, image5, wifi, parking, tv, ac, smoke, electricity, created_at,
-              image, username FROM appartments_list
-                 JOIN users ON appartments_list.userId = users.id 
-                 JOIN watch_list ON watch_list.appartment_id = appartments_list.id
-                 JOIN coordinates ON appartments_list.id = coordinates.appartment_id
-                 JOIN offers ON appartments_list.id = offers.appartment_id
-                 JOIN images ON appartments_list.id = images.appartment_id
-                 WHERE watch_list.userId = ? AND watch_list.appartment_id = ?"
+            "SELECT * FROM {$this->table} WHERE userId = ? AND appartment_id = ?"
         );
         $query->bind_param('ii', $this->userId, $this->appartment_id);
         $query->execute();
@@ -71,10 +63,10 @@ class WatchList
     public function delete()
     {
         $query = $this->conn->prepare(
-            "DELETE FROM watch_list WHERE id = ?"
+            "DELETE FROM {$this->table} WHERE appartment_id = ? AND userId = ?"
         );
 
-        $query->bind_param('i', $this->id);
+        $query->bind_param('ii', $this->appartment_id, $this->userId);
 
         $query->execute();
     }
