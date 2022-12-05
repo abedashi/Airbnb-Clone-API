9a52639d5f5ee6db1a8572a1a0f1440667856bc8
Abed al salam Ashi <ashie@Abeds-MacBook-Air.local>
2022-12-05 10:34:32 +0200

    single appartment

---
+++ api/appartments-list/get_single_user.php
@@ -0,0 +1,41 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+  header('Access-Control-Allow-Methods: GET');
+  header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit('ok');
+  }
+
+  require_once '../../config/Database.php';
+  require_once '../../models/AppartmentsList.php';
+
+    // Connect db
+    $database = new Database();
+    $db = $database->connect();
+  
+    $appartment = new AppartmentsList($db);
+
+    try {
+      // Protect route
+      require '../../config/protect.php';
+      
+      $appartment->userId = htmlspecialchars($_GET['userId']);
+
+      if($result = $appartment->get_single_user()) {
+        $response = [];
+        while ($row = $result->fetch_assoc()) {
+          $response[] = $row;
+        }
+        http_response_code(200);
+        echo json_encode($response); 
+      }
+
+    } catch (Exception $e) {
+      http_response_code(401);
+      echo json_encode(
+        array('message' => $e->getMessage())
+      );
+    }
+?>
